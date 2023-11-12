56b820f34d17fdab5f8d3db07444bc11ce3b6a42
cam <cambo9p.11@gmail.com>
2023-11-12 23:11:37 +1300

    created tcp server

---
+++ comms/info.go
@@ -0,0 +1,33 @@
+package comms
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type config struct {
+	port int    `yaml:"port"`
+	host string `yaml:"host"`
+}
+
+var configFile = "config.yml"
+
+func getConfig() (config, error) {
+	var config config
+
+	file, err := os.ReadFile(configFile)
+	if err != nil {
+		logger.Error(fmt.Sprintf("failed to open file %s", configFile))
+		return config, err
+	}
+
+	err = yaml.Unmarshal(file, &config)
+	if err != nil {
+		logger.Error(fmt.Sprintf("failed to unmarshal contents from %s", configFile))
+		return config, err
+	}
+
+	return config, nil
+}
--- comms/server.go
+++ comms/server.go
@@ -1,14 +1,60 @@
 package comms
 
-import "github.com/Cambo9p/sage/util/logger"
+import (
+	"bufio"
+	"fmt"
+	"net"
 
-func StartServer() error {
-  logger, err := logger.NewLogger("comms") 
-  if err != nil {
-    return err
-  }
+	"go.uber.org/zap"
 
-  logger.Info("starting server")
+	logging "github.com/Cambo9p/sage/util/logger"
+)
 
-  return nil
+var logger *zap.Logger
+
+// handles the stream of words
+func handleConnection(c net.Conn) {
+	logger.Info(fmt.Sprintf("Serving %s\n", c.RemoteAddr().String()))
+
+	scanner := bufio.NewScanner(c)
+
+	for scanner.Scan() {
+		// read a line which should be one or more words
+
+		line := scanner.Text()
+		fmt.Printf(line)
+
+		// words := strings.Fields(line)
+
+		// check if the key words were said and then create an event
+
+	}
+
+}
+
+func StartServer() {
+	logger = logging.NewLogger("comms")
+	logger.Info("starting server")
+
+	newConfig, err := getConfig()
+	port := fmt.Sprintf(":%d", newConfig.port)
+	host := newConfig.host
+
+	logger.Info(fmt.Sprintf("running server on port %s on %s", port, host))
+
+	l, err := net.Listen("tcp4", ":5000")
+	if err != nil {
+		logger.Error(fmt.Sprintf("failed to listen for connections on %s", port))
+	}
+
+	defer l.Close()
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			logger.Error("failed to accept connections on")
+		}
+		go handleConnection(c)
+	}
+
 }
