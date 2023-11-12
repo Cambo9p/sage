6b5934ffb7a1a3c915545705c740b7c0d52f07ec
cam <cambo9p.11@gmail.com>
2023-11-12 23:11:49 +1300

    refactored logger

--- util/logger/logger.go
+++ util/logger/logger.go
@@ -7,23 +7,25 @@ import (
 
 var loggers = make(map[string]*zap.Logger)
 
-func NewLogger(packageName string) (*zap.Logger, error) {
-  if logger, ok := loggers[packageName]; ok {
-    return logger, nil
-  }
+// this logger doesnt handle errors well, we jsut ignore them as making the parent
+// handle them is just plain annoying and increases checks
+func NewLogger(packageName string) *zap.Logger {
+	if logger, ok := loggers[packageName]; ok {
+		return logger
+	}
 
-  config := zap.NewProductionConfig()
-  config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
-  config.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	config := zap.NewProductionConfig()
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	config.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
 
-  config.OutputPaths = []string{"logs/app.log"}
+	config.OutputPaths = []string{"logs/app.log"}
 
-  logger, err := config.Build()
-  if err != nil {
-    return nil, err
-  }
+	logger, err := config.Build()
+	if err != nil {
+		return nil
+	}
 
-  loggers[packageName] = logger
+	loggers[packageName] = logger
 
-  return logger, nil
+	return logger
 }
